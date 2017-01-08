72ffb200e14dd10aa02ad0771730ab6bccc8aa8b
Jeremyywb <gjeremyyou@gmail.com>
2017-01-08 22:04:43 +0800

    Create index.js

---
+++ TimeLine/js/index.js
@@ -0,0 +1,66 @@
+$('.dot:nth-child(1)').click(function(){
+  $('.inside').animate({
+    'width' : '20%'
+  }, 500);
+});
+$('.dot:nth-child(2)').click(function(){
+  $('.inside').animate({
+    'width' : '40%'
+  }, 500);
+});
+$('.dot:nth-child(3)').click(function(){
+  $('.inside').animate({
+    'width' : '60%'
+  }, 500);
+});
+$('.dot:nth-child(4)').click(function(){
+  $('.inside').animate({
+    'width' : '80%'
+  }, 500);
+});
+if ($('#switch1').not(':checked')){
+  $('.modal').wrap('<div class="mask"></div>')
+    $('.mask').click(function(){
+      $(this).fadeOut(300);
+      $('.mask article').animate({
+        'top' : '-100%'
+      }, 300)
+    });
+
+    $('.dot').click(function(){
+      var modal = $(this).attr('id');
+      $('.mask').has('article.' + modal).fadeIn(300);
+      $('.mask article.' + modal).fadeIn(0).animate({
+        'top' : '10%'
+      }, 300);
+    });
+}
+$("#switch1").click(function(){
+  if ($('#switch1').is(':checked')){
+    $('.modal').unwrap('<div class="mask"></div>');
+    $('.modal').hide();
+    $('.modal').addClass('nobox');
+    $('.dot').click(function(){
+    var modal = $(this).attr('id');
+    $('article.nobox').hide()
+    $('article.nobox.' + modal).fadeIn(200)
+	});
+  } else {
+    $('article').removeClass("nobox");
+    $('.modal').wrap('<div class="mask"></div>')
+    $('.mask').click(function(){
+      $(this).fadeOut(300);
+      $('.mask article').animate({
+        'top' : '-100%'
+      }, 300)
+    });
+
+    $('.dot').click(function(){
+      var modal = $(this).attr('id');
+      $('.mask').has('article.' + modal).fadeIn(300);
+      $('.mask article.' + modal).fadeIn(0).animate({
+        'top' : '10%'
+      }, 300);
+    });
+  }
+})
